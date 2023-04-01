49ac612e21df835f7df5e181cd2ef59ed17e3dc0
Sahil Patel <sahpatel21@gmail.com>
2023-04-01 00:11:13 -0400

    Add header and implementation files for testing utilities
    These include a class which maintains the packets to be sent and expected
    to be recieved for a given test, as well as functions for crafting packets
    and maintaining the state of the test.

---
+++ include/testing_utils.hpp
@@ -0,0 +1,60 @@
+/*
+ * testing_utils.hpp - Header file for various testing utilites.
+ *
+ * This contains declarations for a class and various functions to simplify interthread
+ * communication between packet capturing threads created by PcapPlusPlus, as well as helper
+ * functions for crafting packets, evaluating packets as they arrive, and evalutating test results
+ * once all packets have been transmitted and received.
+ */
+
+#ifndef TESTING_UTILS_HPP
+#define TESTING_UTILS_HPP
+
+#include <mutex>
+#include <vector>
+#include <PcapLiveDevice.h>
+#include <RawPacket.h>
+#include "duplicate_manager.hpp"
+
+/*
+ * TestData - This is shared between the all running threads of the testing container program. It
+ * is to be populated with various information about the current running test, most notably: the
+ * the packets to be transmitted out each interface, and the packets which are expected to arrive
+ * on each interface.
+ */
+class TestData {
+public:
+    enum status {FAIL, PASS, IN_PROGRESS};
+
+    TestData(std::vector<pcpp::PcapLiveDevice *> veth_intfs);
+
+    std::vector<std::pair<pcpp::RawPacket, pcpp::PcapLiveDevice *>> pckts_to_transmit;
+    DuplicateManager dup_mgr, expected;
+    std::vector<pcpp::PcapLiveDevice *> veth_intfs;
+    status test_status;
+    std::mutex err_out;
+};
+
+/*
+ * create_broadcast_pckt() - Returns a packet whose source MAC and IPv4 address matches that of the
+ * given interface and whose destinations are the broadcast addresses. This packet also has an
+ * empty payload.
+ */
+pcpp::RawPacket create_broadcast_pckt(pcpp::PcapLiveDevice *src_intf);
+
+/*
+ * verify_packet() - Passed to pcpp::PcapLiveDevice.startCapture(). It is called on every incoming
+ * packet, and verifies that it is not a duplicate (see DuplicateManager) and that is a packet we
+ * expected to see. If the packet is unexpected, it outputs an error message and marks the test as
+ * failed.
+ */
+void verify_packet(pcpp::RawPacket *packet, pcpp::PcapLiveDevice *dev, void *cookie);
+
+/*
+ * evaluate_test_results() - After all test packets have been transmitted and the virtual switch
+ * has bridged them back to the testing container, this verifies that all the packets we expected
+ * to see were observed, and returns the final test result after doing this.
+ */
+TestData::status evaluate_test_results(TestData &data);
+
+#endif // TESTING_UTILS_HPP
---
+++ src/testing_utils.cpp
@@ -0,0 +1,93 @@
+/*
+ * testing_utils.cpp - Implementation file for the TestData class and other various testing utility
+ * functions, all defined in include/testing_utils.hpp
+ */
+
+#include <iostream>
+#include <EthLayer.h>
+#include <IPv4Layer.h>
+#include <SystemUtils.h>
+#include "testing_utils.hpp"
+
+TestData::TestData(std::vector<pcpp::PcapLiveDevice *> veth_intfs)
+    : dup_mgr(veth_intfs.size()),
+      expected(veth_intfs.size()),
+      veth_intfs(veth_intfs),
+      test_status(IN_PROGRESS)
+{}
+
+pcpp::RawPacket create_broadcast_pckt(pcpp::PcapLiveDevice *src_intf) {
+    pcpp::EthLayer eth_layer(src_intf->getMacAddress(),
+			     pcpp::MacAddress("ff:ff:ff:ff:ff:ff"));
+
+    pcpp::IPv4Layer ip_layer(src_intf->getIPv4Address(),
+			     pcpp::IPv4Address("255.255.255.255"));
+    ip_layer.getIPv4Header()->ipId = pcpp::hostToNet16(2000);
+    ip_layer.getIPv4Header()->timeToLive = 64;
+
+    pcpp::Packet pckt(eth_layer.getHeaderLen() + ip_layer.getHeaderLen());
+    pckt.addLayer(&eth_layer);
+    pckt.addLayer(&ip_layer);
+    pckt.computeCalculateFields();
+    return *pckt.getRawPacket();
+}
+
+void verify_packet(pcpp::RawPacket *packet, pcpp::PcapLiveDevice *dev, void *cookie) {
+    TestData *data = static_cast<TestData *>(cookie);
+    pcpp::Packet parsed_packet(packet);
+
+    for(long unsigned int i = 0; i < data->veth_intfs.size(); i++) {
+	if(dev != data->veth_intfs[i]) {
+	    continue;
+	}
+
+	if(data->dup_mgr.check_duplicate(i, *packet)) {
+	    return;
+	}
+
+	if(data->expected.check_duplicate(i, *packet)) {
+	    return;
+	} else {
+	    data->err_out.lock();
+	    std::cerr
+		<< "\tFAIL: Saw unexpected packet on interface "
+		<< dev->getName()
+		<< std::endl;
+
+	    std::vector<std::string> pckt_strings;
+	    parsed_packet.toStringList(pckt_strings);
+	    for(auto s : pckt_strings) {
+		std::cerr << '\t' << s << std::endl;
+	    }
+	    std::cerr << std::endl;
+
+	    data->err_out.unlock();
+	    data->test_status = TestData::FAIL;
+	}
+    }
+    return;
+}
+
+TestData::status evaluate_test_results(TestData &data) {
+    bool all_not_seen = false;
+    for(long unsigned int i = 0; i < data.veth_intfs.size(); i++) {
+	if(data.expected.num_packets_for_intf(i) > 0) {
+	    data.test_status = TestData::FAIL;
+	    all_not_seen = true;
+	    std::cerr
+		<< "\tFAIL: Not all expected packets were seen on interface "
+		<< data.veth_intfs[i]->getName()
+		<< std::endl;
+	}
+    }
+
+    if(all_not_seen) {
+	std::cerr << "\tThe following packets were expected to arrive, but did not:" << std::endl;
+	std::cerr << data.expected.to_string("\t");
+    }
+
+    if(data.test_status == TestData::IN_PROGRESS) {
+	return TestData::PASS;
+    }
+    return TestData::FAIL;
+}
